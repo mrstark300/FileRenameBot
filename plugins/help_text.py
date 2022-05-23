efff2f040a609d04ac4ac389edb49ae6f9fdb299
mrstark300 <89143578+mrstark300@users.noreply.github.com>
2022-05-23 16:28:04 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -102,12 +102,12 @@ async def start_me(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Channel', url='https://t.me/Stark_support'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/tony_strak_3000')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Other Bots', url='https://t.me/Stark_botz'),
+                    InlineKeyboardButton('Source', url='https://t.me/Stark_botz')
                 ]
             ]
         ),
